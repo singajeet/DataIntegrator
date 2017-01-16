11b87b156346fc731fdaf9b059c69c5c8e5648d8
singajeet <singajeet@gmail.com>
2017-01-16 23:30:22 +0530

    Changes_011620171130

--- integrator/node/node_controller.py
+++ integrator/node/node_controller.py
@@ -40,58 +40,60 @@ class constants(Values):
 if __name__ == '__main__':
     logger.info(_('Data Integrator node has been started...'))
 
+    # ####################### Initiate plugin manager ################################################
     logger.info(_('Initiating plug-in manager to load required plug-ins...'))
-    simple_plugin_manager = PluginManager(categories_filter={'Node': interfaces.INode,
-                                                             'DBAuthProvider': interfaces.INodeDBAuthProvider,
-                                                             'DatabaseManager': interfaces.INodeDatabaseManager
-                                                             })
+    plugin_manager = PluginManager(categories_filter={'Node': interfaces.INode,
+                                                      'DBAuthProvider': interfaces.INodeDBAuthProvider,
+                                                      'DatabaseManager': interfaces.INodeDatabaseManager
+                                                      })
     logger.debug(_('Plug-in manager has been started'))
-    simple_plugin_manager.setPluginPlaces(['./integrator/plugins'])
+    plugin_manager.setPluginPlaces([config.plugin_path])
     logger.debug(_('Plugin-in manager will search for plug-ins in folder: /integrator/plugins'))
-    simple_plugin_manager.collectPlugins()
+    plugin_manager.collectPlugins()
 
-    node_plugin = simple_plugin_manager.getPluginByName(constants.NODE_PLUGIN.value, category='Node')
-    db_cred = simple_plugin_manager.getPluginByName(constants.DB_AUTH_PROVIDER_PLUGIN.value, category='DBAuthProvider')
-    db_manager = simple_plugin_manager.getPluginByName(constants.NODE_DB_PLUGIN.value, category='DatabaseManager')
-    # node.load_system_details()
-    # logger.debug(_('System details loaded'))
-    # logger.debug(node.node_details)
+    # ####################### Filter out the plugins required in this program ##########################
+    node = plugin_manager.getPluginByName(constants.NODE_PLUGIN.value, category='Node').plugin_object
+    db_cred = plugin_manager.getPluginByName(constants.DB_AUTH_PROVIDER_PLUGIN.value, category='DBAuthProvider').plugin_object
+    db_manager = plugin_manager.getPluginByName(constants.NODE_DB_PLUGIN.value, category='DatabaseManager').plugin_object
 
-    # v_sys_id = node.sys_id_exists()
-    # if v_sys_id == -1:
-    #     logger.info(_('No existing sys id found'))
-    #     v_sys_id = node.create_node()
-    #     logger.debug(_('sys id saved for this node'))
+    # ####################### Load the system details to be used by Node ###############################
+    node.load_system_details()
+    logger.debug(_('System details loaded'))
+    logger.debug(node.node_details)
 
-    # if not node.validate_sys_id(v_sys_id):
-    #     logger.error(_('Integrity check failed! System will exit now'))
-    #     sys.exit(1)
+    v_sys_id = node.sys_id_exists()
+    if v_sys_id == -1:
+        logger.info(_('No existing sys id found'))
+        v_sys_id = node.create_node()
+        logger.debug(_('sys id saved for this node'))
 
-    # db_cred = INodeDBCredentials()
-    # (user, password) = (None, None)
-    # if not db_cred.credentials_exists():
-    #     logger.debug(_('No db credentials found, user will be prompted for same'))
-    #     (user, password) = db_cred.prompt_credentials()
-    #     logger.debug(_('New db credentials: %s | %s') % (user, password))
-    #     db_cred.save_credentials()
-    #     logger.debug(_('db credentials saved successfully'))
-    # else:
-    #     logger.debug(_('db credentials found and will be loaded'))
-    #     db_cred.load_credentials()
-    #     (user, password) = db_cred.get_credentials()
-    #     logger.debug(_('Credentials loaded successfully: %s %s') % (user, password))
+    if not node.validate_sys_id(v_sys_id):
+        logger.error(_('Integrity check failed! System will exit now'))
+        sys.exit(1)
 
-    # db_manager = INodeDatabaseManager(user, password)
-    # logger.info(_('Node\'s database manager has been started'))
-    # if not db_manager.node_db_exists():
-    #     logger.debug(_('Database is not configured yet and will be initiated now'))
-    # else:
-    #     logger.debug(_('Node database found and will be initialized'))
-    # db_manager.open_or_create_database()
+    (user, password) = (None, None)
+    if not db_cred.credentials_exists():
+        logger.debug(_('No db credentials found, user will be prompted for same'))
+        (user, password) = db_cred.prompt_credentials()
+        logger.debug(_('New db credentials: %s | %s') % (user, password))
+        db_cred.save_credentials()
+        logger.debug(_('db credentials saved successfully'))
+    else:
+        logger.debug(_('db credentials found and will be loaded'))
+        db_cred.load_credentials()
+        (user, password) = db_cred.get_credentials()
+        logger.debug(_('Credentials loaded successfully: %s %s') % (user, password))
 
-    # if not db_manager.node_details_exists(v_sys_id):
-    #     db_manager.save_node_details(node.get_node_details())
-    # else:
-    #     db_manager.load_node_details(v_sys_id)
+    logger.info(_('Node\'s database manager has been started'))
+    if not db_manager.node_db_exists():
+        logger.debug(_('Database is not configured yet and will be initiated now'))
+    else:
+        logger.debug(_('Node database found and will be initialized'))
+    db_manager.open_or_create_database()
 
-    # logger.debug(_('Database configured successfully!'))
+    if not db_manager.node_details_exists(v_sys_id):
+        db_manager.save_node_details(node.get_node_details())
+    else:
+        db_manager.load_node_details(v_sys_id)
+
+    logger.debug(_('Database configured successfully!'))
